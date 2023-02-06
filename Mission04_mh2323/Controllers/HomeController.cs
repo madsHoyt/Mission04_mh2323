b0f24f8216a1898b8d9aaa0b3a229e838c565190
madHoyt <madisonhoyt19@gmail.com>
2023-02-06 15:14:21 -0700

    mission 5

---
+++ Mission04_mh2323/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿//This file is my HomeController.cs file 
+using Microsoft.AspNetCore.Mvc;
+using Mission04_mh2323.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_mh2323.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //method for get
+        [HttpGet]
+        public IActionResult calc()
+        {
+            return View();
+        }
+
+        //method for post
+        [HttpPost]
+        public IActionResult calc(calcModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission04_mh2323/Models/calcModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_mh2323.Models
+{
+    public class calcModel
+    {
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
+        public int Assignment { get; set; }
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
+        public int GroupProject { get; set; }
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
+        public int Quiz { get; set; }
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
+        public int Midterm { get; set; }
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
+        public int Final { get; set; }
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
+        public int Intex { get; set; } 
+    }
+}
